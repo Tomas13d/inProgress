ae62119970643b5ff088cb5cf88b49ca28da622a
Tomas13d <tomas.demobio@gmail.com>
2022-04-21 12:38:18 -0300

    sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-
-
+import {RiWhatsappLine, RiGithubLine, RiLinkedinLine, RiInstagramLine, RiFileTextLine } from "react-icons/ri";
 
 function App() {
   return (
@@ -44,8 +43,24 @@ function App() {
         <div class="fluid"></div>
    </div>
    <div>
-    <h1 class="in">In Progress...</h1>  
-</div>
+   <h1 class="in">In Progress...</h1>  
+   </div>
+
+    <div className="social-media-container">
+            <a href="https://linkedin.com/in/tomas-demo"><RiLinkedinLine/></a>
+            <a href="https://github.com/Tomas13d"><RiGithubLine/></a>
+            <a href="https://ultramsg.com/m/3va2ZdI"><RiWhatsappLine/></a>
+            <a href="https://www.instagram.com/tomasjdemo/"><RiInstagramLine/></a>
+            <a href="#"><RiFileTextLine/></a>
+    </div>
+
+     <div className="mail-container">
+     <div className="mail">
+        <a href="mailto:tomas.demobio@gmail.com?subject=I saw your profile">tomas.demobio@gmail.com</a>
+        
+     </div>
+     </div>
+
 </>
   );
 }
